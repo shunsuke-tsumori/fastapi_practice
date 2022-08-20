4b572709dfe289ebf6b9b2cdfff1f5da92cecd5d
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 19:12:35 +0900

    hello http status

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
+from sqlalchemy.orm import Session
 
 from . import models
-from .schemas import Blog
-from .models import Base
 from .database import engine, session_local
-from sqlalchemy.orm import Session
+from .models import Base
+from .schemas import Blog
 
 app = FastAPI()
 
@@ -21,7 +21,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def post_blog(blog: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -30,13 +30,15 @@ def post_blog(blog: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
+@app.get("/blog", status_code=status.HTTP_200_OK)
 def all_getch(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
 def show(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
     return blog
