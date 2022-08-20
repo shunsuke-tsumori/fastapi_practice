22532004ae6c92a41f141c3682cd9fc3de2316ef
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 16:51:45 +0900

    hello request params

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ def index():
 @app.get("/blog/{id}/comments")
 def get_comments(id: int):
     return {"data": {id, "comments"}}
+
+
+@app.get("/blog")
+def get_item(limit: int = 10, published: bool = True):
+    if published:
+        return {"data": f"{limit}件"}
+    else:
+        return {"data": "not found"}
