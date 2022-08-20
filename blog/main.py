728e5cbabdc1e4c295d4f9188f988e4dc556740a
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 18:59:25 +0900

    hello db read

--- blog/main.py
+++ blog/main.py
@@ -28,3 +28,15 @@ def post_blog(blog: Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog")
+def all_getch(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}")
+def show(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
