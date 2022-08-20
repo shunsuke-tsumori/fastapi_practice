b8c2bf7be77ba86a50c06261af3d3816ce67e6d5
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 20:15:43 +0900

    hello db update (orm_mode does not work)

--- blog/main.py
+++ blog/main.py
@@ -52,3 +52,14 @@ def delete(id: int, db: Session = Depends(get_db)):
     blog.delete(synchronize_session=False)
     db.commit()
     return "Deletion completed"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+    blog.update(request.dict())
+    db.commit()
+
+    return "Update completed"
