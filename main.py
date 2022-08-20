cf4be8ded520895b7f00300563b7f90f07616cae
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 17:05:58 +0900

    hello post

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import pydantic
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -22,3 +25,14 @@ def get_item(limit: int = 10, published: bool = True):
         return {"data": f"{limit}件"}
     else:
         return {"data": "not found"}
+
+
+class Blog(pydantic.BaseModel):
+    title: str
+    description: str
+    published_at: Optional[bool]
+
+
+@app.post("/blog")
+def post_blog(blog: Blog):
+    return {"data": blog}
