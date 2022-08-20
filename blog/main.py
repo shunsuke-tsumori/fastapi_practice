dfd02ba3cdf9dfb66a5a6ad4f37e96152271a77c
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2022-08-20 19:21:33 +0900

    hello db delete

--- blog/main.py
+++ blog/main.py
@@ -42,3 +42,13 @@ def show(id: int, db: Session = Depends(get_db)):
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
     return blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "Deletion completed"
